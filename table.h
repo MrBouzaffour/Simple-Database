4b2da068d8991c66f7e3d34b4fd41befbbdbffe2
ahmed <ahmedbouzaffour2002@gmail.com>
2024-05-23 08:22:06 +0000

    Cleaned the code for table.h

--- table.h
+++ table.h
@@ -4,19 +4,17 @@
 #include <stdint.h>
 
 
-
 /* ------ Row ------ */
 
 #define COLUMN_USERNAME_SIZE 32 // Maximum length of username string
 #define COLUMN_EMAIL_SIZE 255   // Maximum length of email string
 
-
 // Row structure
 typedef struct
 {
-    uint32_t id;    // Unique identifier for the row.
+    uint32_t id;                            // Unique identifier for the row.
     char username[COLUMN_USERNAME_SIZE];    // Buffer for storing the username, fixed length.
-    char email[COLUMN_EMAIL_SIZE];      // Buffer for storing the email, fixed length.
+    char email[COLUMN_EMAIL_SIZE];          // Buffer for storing the email, fixed length.
 } Row;
 
 /* ------ Row Layout ------ */
